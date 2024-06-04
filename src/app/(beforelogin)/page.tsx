2518103f6081d38d20af03ad7ab0542f4772e6e9
jin <wonjin9497@gmail.com>
2024-06-04 18:02:16 +0900

    go dirty css

--- src/app/(beforelogin)/page.tsx
+++ src/app/(beforelogin)/page.tsx
@@ -11,18 +11,16 @@ export default function BeforeLoginDefaultPage() {
                 <Image src={zLogo} alt="logo" />
             </div>
             <div className={styles.right}>
-                <div>
-                    <h1>지금 일어나고 있는 일</h1>
-                </div>
-                <div>
-                    <h2>지금 가입하세요.</h2>
-                </div>
+                <h1>지금 일어나고 있는 일</h1>
+
+                <h2>지금 가입하세요.</h2>
+
                 <Link href={Route.IFlowSignUp} className={styles.signup}>
                     계정 만들기
                 </Link>
-                <div>
-                    <h3>이미 트위터에 가입하셨나요?</h3>
-                </div>
+
+                <h3>이미 트위터에 가입하셨나요?</h3>
+
                 <Link href={Route.Login} className={styles.login}>
                     로그인
                 </Link>
