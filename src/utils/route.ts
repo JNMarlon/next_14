45c92971ce93c075e0f67168ea92f6eaf8da3e79
jin <wonjin9497@gmail.com>
2024-06-04 17:28:06 +0900

    separate static routes

---
+++ src/utils/route.ts
@@ -0,0 +1,9 @@
+export const enum Route {
+    Messages = '/messages',
+    Search = '/search',
+    Explore = '/explore',
+    Tweet = '/tweet',
+    Login = '/login',
+    IFlowLogin = '/i/flow/login',
+    IFlowSignUp = '/i/flow/signup',
+}
