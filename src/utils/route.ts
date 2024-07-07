d8f98eb34519af4705ea649016e55f6617cae2d5
JNMarlon <wonjin6368@naver.com>
2024-07-07 17:18:38 +0900

    add routes

--- src/utils/route.ts
+++ src/utils/route.ts
@@ -2,8 +2,10 @@ export const enum Route {
     Messages = '/messages',
     Search = '/search',
     Explore = '/explore',
+    ComposeTweet = '/compose/tweet',
     Tweet = '/tweet',
     Login = '/login',
+    Home = '/home',
     IFlowLogin = '/i/flow/login',
     IFlowSignUp = '/i/flow/signup',
 }
